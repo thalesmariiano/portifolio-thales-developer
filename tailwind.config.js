8c1a62399b88c4e69bc2a6a942abe0c15585e87f
Thales Mariano <thales.mariano125@gmail.com>
2023-06-12 10:25:16 -0300

    fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
     extend: {},
+    fontFamily: {
+      madeTommy: ['madeTommyLight', 'sans-serif'],
+      kusanagi: ['kusanagi', 'sans-serif']
+    }
   },
   plugins: [],
 }
