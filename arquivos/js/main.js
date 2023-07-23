7877135c323e57424d6a03c1a824ba8418a104aa
Thales Mariano <thales.mariano125@gmail.com>
2023-07-23 19:11:51 -0300

    new script with animation to open menu

--- arquivos/js/main.js
+++ arquivos/js/main.js
@@ -1,14 +1,34 @@
-
-const mobileMenu      = document.querySelector("#mobile-menu")
-const openMobileMenu  = document.querySelector("#open-mobile-menu")
-const closeMobileMenu = document.querySelector("#close-mobile-menu")
+const mobileMenu          = document.querySelector("#mobile-menu")
+const openMobileMenu      = document.querySelector("#open-mobile-menu")
+const closeMobileMenu     = document.querySelector("#close-mobile-menu")
+const mobileMenuContainer = document.querySelector("#mobile-menu-container")
 
 openMobileMenu.addEventListener("click", () => {
-	mobileMenu.classList.remove("hidden")
+	addAnimation(mobileMenuContainer, "animate__fadeIn")
+	addAnimation(mobileMenu, "animate__fadeInRight")
 })
 
 closeMobileMenu.addEventListener("click", () => {
-	mobileMenu.classList.add("hidden")
+	removeAnimation(mobileMenuContainer, "animate__fadeOut")
+	removeAnimation(mobileMenu, "animate__fadeOutRight")
 })
 
-window.resize = () => mobileMenu.classList.add("hidden")
+const addAnimation = (element, animation) => {
+	element.classList.remove("hidden")
+	element.classList.add(animation, "animate__animated")
+
+	element.addEventListener("animationend", () => {
+		element.classList.remove(animation, "animate__animated")
+	})
+}
+
+const removeAnimation = (element, animation) => {
+	element.classList.add(animation, "animate__animated")
+
+	element.addEventListener("animationend", () => {
+		element.classList.add("hidden")
+		element.classList.remove(animation, "animate__animated")
+	})
+}
+
+window.resize = () => mobileMenuContainer.classList.add("hidden")
