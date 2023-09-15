39c885ed2f734cd3cde1a7c4482fc21f3c48f43d
Thales Mariano <thales.mariano125@gmail.com>
2023-09-14 23:13:44 -0300

    fix: removed extend duplicated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mediumPurple: '#551fdb',
+        lightPurple: '#6531e6',
+        mediumDarkPurple: '#02002d',
+        darkPurple: '#020026',
+        veryDarkPurple: '#04021d'
+      },
+      boxShadow: {
+        inset: 'inset -3px 5px 5px rgba(0,0,0,.2)',
+        toLeft: '-3px 5px 5px rgba(0,0,0,.2)'
+      }
+    },
     fontFamily: {
       madeTommy: ['madeTommyLight', 'sans-serif'],
       kusanagi: ['kusanagi', 'sans-serif']
